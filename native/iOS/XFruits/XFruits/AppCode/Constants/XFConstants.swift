f3543aac41934f19138b686fe940b7ca0197d12b
kunyintang <iam@tangkunyin.com>
2017-04-22 22:38:16 +0800

    规范常量

--- native/iOS/XFruits/XFruits/AppCode/Constants/XFConstants.swift
+++ native/iOS/XFruits/XFruits/AppCode/Constants/XFConstants.swift
@@ -8,49 +8,83 @@
 
 import UIKit
 
-struct Constants {
+/**
+ *   所有全局常量均在此处定义
+ *   使用方式如：XFConstants.UI.deviceWidth 即可取得设备宽
+ */
+struct XFConstants {
     
-    // 通用UI尺寸
+    
+    /// 开发模式
+    static let isDevelopModel:Bool = true
+    
+    /// 服务器连接超时时间
+    static let serverTimeout:Double = 45.0
+    
+    /// API服务器地址
+    static let appServer:String = "http://www.shuoit.net"
+    
+    /// DES3 KEY (必须是24位)
+    static let desKey:String = "123456781234567812345678"
+    
+    /// MD5 KEY (8位)
+    static let md5Key:String = "88888888"
+    
+    /// 分页每页的条数
+    static let pageRows:Int = 10
+    
+    
+    /// 事件消息key定义
+    struct MessageKey {
+        
+        //网络请求失败
+        static let serverRequestFailure: String = "ServerRequestFailure"
+        
+        //轮播图点击事件
+        static let scrollImageViewClicked: String = "ScrollImageViewClicked"
+    }
+    
+    
+    /// 通用UI尺寸
     struct UI {
         
         static let deviceWidth: CGFloat = UIScreen.main.bounds.size.width
         
         static let deviceHeight: CGFloat = UIScreen.main.bounds.size.height
         
-        static let FullScreenRect: CGRect = CGRect(x: 0, y: 0, width: deviceWidth, height: deviceHeight)
+        static let fullScreenRect: CGRect = CGRect(x: 0, y: 0, width: deviceWidth, height: deviceHeight)
         
-        static let FullVCRect: CGRect = CGRect(x: 0, y: 0, width: deviceWidth, height: deviceHeight - 64)
+        static let fullVCRect: CGRect = CGRect(x: 0, y: 0, width: deviceWidth, height: deviceHeight - 64)
         
-        static let FullTabbarVCRect: CGRect = CGRect(x: 0, y: 0, width: deviceWidth, height: deviceHeight - 64 - 49)
+        static let fullTabbarVCRect: CGRect = CGRect(x: 0, y: 0, width: deviceWidth, height: deviceHeight - 64 - 49)
         
-        static let SINGLE_LINE_ADJUST_OFFSET: CGFloat = ((1 / UIScreen.main.scale) / 2)
+        static let singleLineAdjustOffset: CGFloat = ((1 / UIScreen.main.scale) / 2)
         
-        static let Rectangle_H40: CGRect = CGRect(x: 0, y: 0, width: deviceWidth, height: 40)
-        
-        static let Rectangle_H40_span10: CGRect = CGRect(x: 0, y: 0, width: deviceWidth - 20, height: 40)
     }
     
-    // 通用视觉规范
+    /// 通用视觉规范
     struct Color {
-        static let BlueColor: UIColor = colorWithRGB(0, g: 153, b: 255) // 主色调（蓝色）
-        static let OrangeColor: UIColor = colorWithRGB(242, g: 109, b: 95) // 辅色调（橙色）
-        static let YellowColor: UIColor = colorWithRGB(239, g: 166, b: 68) // 辅色调（黄色）
-        static let WhiteColor: UIColor = grayColor(255) // 纯白
-        static let BlackColor: UIColor = grayColor(53) // 标题文字（浅黑色）
-        static let GrayColor: UIColor = grayColor(102) // 正文文字（灰色）
-        static let LightGrayColor: UIColor = grayColor(153) // 标注文字（浅灰色）
-        static let CommonBgColor: UIColor = grayColor(248) // 背景色
-        static let SeparatorLineColor: UIColor = grayColor(232) // 分割线颜色
-        static let TranslucentColor: UIColor = colorWithRGB(0, g: 0, b: 0, alpha: 0.55) // 半透明颜色
+        static let whiteColor:UIColor = colorWithRGB(255, g: 255, b: 255)          //纯白
+        static let blueColor:UIColor = colorWithRGB(0, g: 153, b: 255)             //蓝色
+        static let orangeColor:UIColor = colorWithRGB(253, g: 106, b: 60)          //橙色
+        static let yellowColor:UIColor = colorWithRGB(239, g: 166, b: 68)          //黄色
+        static let blackColor:UIColor = colorWithRGB(68, g: 68, b: 68)             //浅黑色
+        static let grayColor:UIColor = colorWithRGB(102, g: 102, b: 102)           //灰色
+        static let lightGrayColor:UIColor = colorWithRGB(153, g:153, b:153)        //浅灰色
+        static let coffeeColor:UIColor = colorWithRGB(185,g: 152,b: 99)            //咖啡色
+        static let commonBgColor:UIColor = colorWithRGB(248, g: 248, b: 248)       //灰白色
+        static let separatorLineColor:UIColor = colorWithRGB(232, g: 232, b: 232)  //分割线颜色
+        static let translucentColor:UIColor = colorWithRGB(0,g: 0,b: 0, alpha: 0.55)//半透明颜色
     }
     
-    // 通用字体
+    /// 通用字体
     struct Font {
-        static let MainBodyFont: UIFont = htscFontWithSize(12)
-        static let MainMenuFont: UIFont = htscFontWithSize(17)
-        static let TitleFont: UIFont = htscFontWithSize(14)
-        static let BottomMenuFont: UIFont = htscFontWithSize(11)
+        static let mainBodyFont: UIFont = htscFontWithSize(12)
+        static let mainMenuFont: UIFont = htscFontWithSize(17)
+        static let titleFont: UIFont = htscFontWithSize(14)
+        static let bottomMenuFont: UIFont = htscFontWithSize(11)
     }
     
 }
+
 
--- native/iOS/XFruits/XFruits/AppCode/Constants/XFUIFuncDefines.swift
+++ native/iOS/XFruits/XFruits/AppCode/Constants/XFUIFuncDefines.swift
@@ -1,14 +1,31 @@
 //
-//  XFFunsDefine.swift
+//  XFUIFuncDefines.swift
 //  XFruits
 //
-//  Created by tangkunyin on 2017/4/16.
+//  Created by tangkunyin on 2017/4/22.
 //  Copyright © 2017年 www.10fruits.net. All rights reserved.
 //
 
 import UIKit
 
-//: 全局颜色函数定义
+//  全局UI相关工具函数定义
+func kVCWidth(_ obj:UIViewController) -> CGFloat {
+    return obj.view.frame.size.width
+}
+
+func kVCHeight(_ obj:UIViewController) -> CGFloat {
+    return obj.view.frame.size.height
+}
+
+func kViewWidth(_ obj:UIView) -> CGFloat {
+    return obj.frame.size.width
+}
+
+func kViewHeight(_ obj:UIView) -> CGFloat {
+    return obj.frame.size.width
+}
+
+//: 全局颜色工具函数定义
 func colorWithRGB(_ r:CGFloat,g:CGFloat,b:CGFloat) -> UIColor {
     return colorWithRGB(r, g: g, b: b, alpha: 1)
 }
@@ -17,20 +34,26 @@ func colorWithRGB(_ r:CGFloat,g:CGFloat,b:CGFloat,alpha:CGFloat) -> UIColor {
     return UIColor.init(red: r/255.0, green: g/255.0, blue: b/255.0, alpha: alpha)
 }
 
+func colorWithHexValue(_ hexValue:Int) -> UIColor {
+    let _r:CGFloat = CGFloat(Float((hexValue & 0xFF0000) >> 16))
+    let _g:CGFloat = CGFloat(Float((hexValue & 0xFF00) >> 16))
+    let _b:CGFloat = CGFloat(Float((hexValue & 0xFF) >> 16))
+    return colorWithRGB(_r, g: _g, b: _b, alpha: 1)
+}
+
 func grayColor(_ rgb:CGFloat) -> UIColor {
     return colorWithRGB(rgb, g: rgb, b: rgb, alpha: 1)
 }
 
+
 func randomColorWithAlpha(_ alpha:CGFloat) -> UIColor {
-    
     let rValue:CGFloat = CGFloat(Float(arc4random_uniform(255))/255.0)
     let gValue:CGFloat = CGFloat(Float(arc4random_uniform(255))/255.0)
     let bValue:CGFloat = CGFloat(Float(arc4random_uniform(255))/255.0)
-    
     return colorWithRGB(rValue, g: gValue, b: bValue, alpha: alpha)
 }
 
-//: 全局颜色函数定义
+//: 全局字体函数定义
 func sysFontWithSize(_ size:CGFloat) -> UIFont {
     return UIFont.systemFont(ofSize: size)
 }
