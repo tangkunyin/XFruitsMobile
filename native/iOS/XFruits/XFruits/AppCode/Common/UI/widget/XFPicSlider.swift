a303f8deabf1bdf4161bab507ad8b90223841f1f
kunyintang <iam@tangkunyin.com>
2017-04-23 21:29:25 +0800

    增加幻灯片组件

---
+++ native/iOS/XFruits/XFruits/AppCode/Common/UI/widget/XFPicSlider.swift
@@ -0,0 +1,38 @@
+//
+//  XFPicSlider.swift
+//  XFruits
+//
+//  Created by tangkunyin on 2017/4/23.
+//  Copyright © 2017年 www.10fruits.net. All rights reserved.
+//
+
+import UIKit
+
+public enum PageControlStyle {
+    case centerPager
+    case rightPager
+}
+
+class XFPicSlider: UIView {
+
+    var style:PageControlStyle //设置PageControl位置 default is PageControlAtCenter
+    
+    func initWithFrame(_ frame:CGRect, placeHolderImage imgName:String) -> XFPicSlider! {
+    
+    
+    
+    
+        return nil
+    }
+    
+    class func  picScrollViewWithFrame(_ frame:CGRect, images:Array<String>) -> XFPicSlider! {
+    
+    
+        return nil
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
