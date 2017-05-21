2a64dfba16c3cb5d90b2d221f83354d0aef56c71
tangsir <iam@tangkunyin.com>
2017-05-21 17:41:19 +0800

    果篮布局

--- native/iOS/XFruits/XFruits/AppCode/Business/ShopCar/View/XFShopCartActionBar.swift
+++ native/iOS/XFruits/XFruits/AppCode/Business/ShopCar/View/XFShopCartActionBar.swift
@@ -10,12 +10,10 @@ import UIKit
 
 class XFShopCartActionBar: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    convenience init(textColor:UIColor?, selectTextColor:UIColor?) {
+        self.init()
+        
+        backgroundColor = XFConstants.Color.salmon
     }
-    */
 
 }
--- native/iOS/XFruits/XFruits/AppCode/Business/ShopCar/XFruitsShopCarViewController.swift
+++ native/iOS/XFruits/XFruits/AppCode/Business/ShopCar/XFruitsShopCarViewController.swift
@@ -7,16 +7,81 @@
 //
 
 import UIKit
+import MBProgressHUD
 
-class XFruitsShopCarViewController: XFruitsBaseViewController {
+fileprivate let XFCartCellReuseIdentifier:String = "XFShopCartCellReuseIdentifier"
+
+class XFruitsShopCarViewController: XFruitsBaseViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    lazy var cartListView:UITableView = {
+        let listView = UITableView.init(frame: CGRect.zero, style: .plain)
+        listView.delegate = self
+        listView.dataSource = self
+        listView.backgroundColor = UIColor.white
+        listView.register(XFShopCartViewCell.self, forCellReuseIdentifier: XFCartCellReuseIdentifier)
+        return listView
+    }()
+    
+    
+    lazy var actionBar:XFShopCartActionBar = {
+        let bar = XFShopCartActionBar(textColor: nil, selectTextColor: nil)
+        return bar;
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "编辑",
+                                                                 style: .plain,
+                                                                 target: self,
+                                                                 action: #selector(onShopCartEdit))
+        view.addSubview(cartListView)
+        view.addSubview(actionBar)
+        makeViewConstrains()
+    }
     
-        
+    fileprivate func makeViewConstrains(){
+        cartListView.snp.makeConstraints { (make) in
+            make.top.equalTo(0)
+            make.left.right.equalTo(self.view)
+            make.bottom.equalTo(self.actionBar.snp.top).offset(-5)
+        }
+        actionBar.snp.makeConstraints { (make) in
+            make.height.equalTo(40)
+            make.top.equalTo(self.cartListView.snp.bottom).offset(5)
+            make.left.right.equalTo(self.view)
+            make.bottom.equalTo(self.view)
+        }
     }
 
+    
+    @objc private func onShopCartEdit(){
+        MBProgressHUD.showError("您有毛线可编辑吗？")
+    }
+    
+    
+    // MARK: - Cart Tableview delegates
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: XFCartCellReuseIdentifier, for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row) - 行"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
     
 
 }
