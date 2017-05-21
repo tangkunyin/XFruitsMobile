e29b6aee71af905a287dc04169098b913f26a72c
tangsir <iam@tangkunyin.com>
2017-05-21 22:34:41 +0800

    粗画分类筛选器

--- native/iOS/XFruits/XFruits/AppCode/Business/FruitsCategory/View/XFCategoryHeadSizer.swift
+++ native/iOS/XFruits/XFruits/AppCode/Business/FruitsCategory/View/XFCategoryHeadSizer.swift
@@ -7,13 +7,53 @@
 //
 
 import UIKit
+import SnapKit
 
 class XFCategoryHeadSizer: UIView {
 
+    lazy var sizer:UISegmentedControl = {
+        let segmente = UISegmentedControl(items: ["综合","销量","新品","价格"])
+        segmente.selectedSegmentIndex = 0
+        segmente.apportionsSegmentWidthsByContent = true
+        segmente.tintColor = UIColor.white
+        segmente.addTarget(self, action: #selector(sizerChangedAction(_:)), for: .valueChanged)
+        return segmente
+    }()
+    
+    lazy var bottomLine:UIView = {
+        let line = UIView();
+        line.backgroundColor = grayColor(204)
+        return line
+    }()
+    
     convenience init(textColor:UIColor?, selectTextColor:UIColor?) {
         self.init()
         
-        backgroundColor = UIColor.blue
+        let normalAttributes = [NSForegroundColorAttributeName:textColor ?? grayColor(102),
+                                NSFontAttributeName:pfnFontWithSize(14)]
+        let selectAttributes = [NSForegroundColorAttributeName:selectTextColor ?? colorWithRGB(255, g: 102, b: 102),
+                                NSFontAttributeName:pfnFontWithSize(14)]
+        
+        sizer.setTitleTextAttributes(normalAttributes, for: .normal)
+        sizer.setTitleTextAttributes(selectAttributes, for: .selected)
+        
+        
+        addSubview(sizer)
+        addSubview(bottomLine)
+        sizer.snp.makeConstraints { (make) in
+            make.top.left.right.equalTo(self)
+            make.bottom.equalTo(self.bottomLine.snp.top)
+        }
+        bottomLine.snp.makeConstraints { (make) in
+            make.top.equalTo(self.sizer.snp.bottom)
+            make.height.equalTo(0.6)
+            make.left.right.equalTo(self)
+        }
+    }
+    
+    @objc private func sizerChangedAction(_ segment:UISegmentedControl){
+        let status:Int = segment.selectedSegmentIndex + 1;
+        print("\(status)")
     }
 
 }
