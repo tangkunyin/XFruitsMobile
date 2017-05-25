64957d244e88d00fa9d084c3eca0c45b3d4a686b
tangsir <iam@tangkunyin.com>
2017-05-25 13:54:03 +0800

    粗画果篮底部

--- native/iOS/XFruits/XFruits/AppCode/Business/ShopCar/XFruitsShopCarViewController.swift
+++ native/iOS/XFruits/XFruits/AppCode/Business/ShopCar/XFruitsShopCarViewController.swift
@@ -24,7 +24,7 @@ class XFruitsShopCarViewController: XFruitsBaseViewController,UITableViewDelegat
     
     
     lazy var actionBar:XFShopCartActionBar = {
-        let bar = XFShopCartActionBar(textColor: nil, selectTextColor: nil)
+        let bar = XFShopCartActionBar()
         return bar;
     }()
     
