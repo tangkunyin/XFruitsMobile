e39d6107eaa8850119690c3607603731ea261bbb
tangsir <iam@tangkunyin.com>
2017-05-29 15:53:41 +0800

    增加通用颜色

--- native/iOS/XFruits/XFruits/AppCode/Constants/XFConstants.swift
+++ native/iOS/XFruits/XFruits/AppCode/Constants/XFConstants.swift
@@ -92,6 +92,7 @@ struct XFConstants {
         static let silver:UIColor = colorWithRGB(199,g: 199,b: 205)//银色
         static let greyishBrown:UIColor = colorWithRGB(83,g: 83,b: 83)//浅黑色
         static let salmon:UIColor = colorWithRGB(255,g: 102,b: 102)//苹果红
+        static let pinkishGrey:UIColor = colorWithRGB(204, g: 204, b: 204)//分割线灰色
     }
     
     /// 通用字体
