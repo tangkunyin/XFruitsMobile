53567514e0ee7d65294e6752a8c2c1fefffc4544
tangsir <iam@tangkunyin.com>
2017-06-04 10:46:32 +0800

    更改状态码命名规范

--- native/iOS/XFruits/XFruits/AppCode/Constants/XFEnum.swift
+++ native/iOS/XFruits/XFruits/AppCode/Constants/XFEnum.swift
@@ -10,31 +10,31 @@ import Foundation
 
 
 // 网络状态
-public enum FSNetStateCode {
-    case FSNetStatusReachable       //有网络
-    case FSNetStatusNotReachable    //无网络
+public enum XFNetStateCode {
+    case reachable       //有网络
+    case notReachable    //无网络
 }
 
 
 // HTTP返回码
-public enum FSHttpStatusCode:Int {
+public enum XFHttpStatusCode:Int {
     //通用状态码
-    case HttpStatusSuccessCode = 200           //请求成功
-    case HttpStatusNotModifyCode = 304         //资源未修改
-    case HttpStatusUnAuthoriztionCode = 401    //请求未授权
-    case HttpStatusForbiddenCode = 403         //禁止访问
-    case HttpStatusNotFoundCode = 404          //资源不存在
-    case HttpStatusReqMethodErrorCode = 405    //请求方法错误。例如要求用POST请求，用了GET
-    case HttpStatusServerErrorCode = 500       //服务器内部错误
+    case success = 200           //请求成功
+    case notModify = 304         //资源未修改
+    case unAuthoriztion = 401    //请求未授权
+    case forbidden = 403         //禁止访问
+    case notFound = 404          //资源不存在
+    case reqMethodError = 405    //请求方法错误。例如要求用POST请求，用了GET
+    case serverError = 500       //服务器内部错误
     
     //自定义状态码(可根据接口实际情况更改)
-    case DataParseErrorCode = -1               //数据解析错误
-    case HttpStatusReturnNullCode = 0          //服务返回为空
-    case HttpStatusFalseCode = 1000            //请求失败(可能是未知原因)
-    case HttpStatusTrueCode = 1001             //请求成功
-    case HttpStatusMissingArgsCode = 1002      //请求缺少参数，或参数不符合要求
-    case HttpStatusVerCodeErrorCode = 1003     //验证码错误
-    case HttpStatusMissingSignCode = 1004      //缺少签名或签名字段错误
-    case HttpStatusSignErrorCode = 1005        //签名错误
-    case HttpStatusUploadErrorCode = 1006      //上传文件失败
+    case dataParseError = -1     //数据解析错误
+    case returnNull = 0          //服务返回为空
+    case returnFalse = 1000      //请求失败(可能是未知原因)
+    case returnTrue = 1001       //请求成功
+    case missingArgs = 1002      //请求缺少参数，或参数不符合要求
+    case verifyError = 1003      //验证码错误
+    case missingSign = 1004      //缺少签名或签名字段错误
+    case signError = 1005        //签名错误
+    case uploadError = 1006      //上传文件失败
 }
---
+++ native/iOS/XFruits/XFruits/AppCode/Utils/XFruitsNetworking.swift
@@ -0,0 +1,13 @@
+//
+//  XFruitsNetworking.swift
+//  XFruits
+//
+//  Created by tangkunyin on 04/06/2017.
+//  Copyright © 2017 www.10fruits.net. All rights reserved.
+//
+
+import UIKit
+
+class XFruitsNetworking: NSObject {
+
+}
